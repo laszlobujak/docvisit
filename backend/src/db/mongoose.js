cd6beb07677df21e6517f475e986c51b85ec072b
laszlobujak <laszlobujak@gmail.com>
2020-03-09 20:58:18 +0100

    Added the connector for the MongoDB with MongooseJS.

---
+++ backend/src/db/mongoose.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URL, {
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useNewUrlParser: true
+});
