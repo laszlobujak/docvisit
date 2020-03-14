7ec1c671e252ed101935844b7e5ce5470767e716
laszlobujak <laszlobujak@gmail.com>
2020-03-14 08:34:22 +0100

    Added Doctors and Appointments MongoDB models

---
+++ backend/src/models/Appointment.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const appointmentSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    doctor: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Doctor'
+    },
+    patient: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    },
+    date: {
+      type: Date,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Appointment = mongoose.model('Appointment', appointmentSchema);
+
+module.exports = Appointment;
---
+++ backend/src/models/Doctor.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const doctorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+userSchema.virtual('appointments', {
+  ref: 'Appointment',
+  localField: '_id',
+  foreignField: 'doctor'
+});
+
+const Doctor = mongoose.model('Doctor', doctorSchema);
+
+module.exports = Doctor;
