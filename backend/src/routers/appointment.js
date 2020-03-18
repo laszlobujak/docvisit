6b6812630d9036e8bc893f80dd2cbb7ea6e88f25
imcsi97 <papim971206@gmail.com>
2020-03-18 23:24:05 +0100

    Update routers/appointment.js
    Add get, patch, delete
    Update Post

--- backend/src/routers/appointment.js
+++ backend/src/routers/appointment.js
@@ -1,16 +1,55 @@
-const express = require('express');
-const AppointmentRouter = express.Router();
+var express = require('express')
+var router = express.Router()
 const Appointment = require('../models/Appointment')
+const User = require('../models/User')
 
-AppointmentRouter.route('/create').post(function(req, res){
-    const appointment = new Appointment(req.body);
-    appointment.save()
-        .then(appointment => {
-            res.json('Appointment added successfully');
+router.get('/appointment/:userid', async (req, res) => { //Get all appointments by UserId
+    var userid = req.params.userid;
+    User.find({ userId: userid }, function (err, tasks) {
+        if (err)
+            res.send(err);
+
+        res.json(tasks);
+
+    })
+})
+
+router.patch('appointment/:id', async (req, res, next) => { //Update an appiontment by id
+    Appointment.update({ appointment: req.body.appointment }, { where: { id: req.params.id } })
+        .then((result) => {
+            res.json(result);
         })
-        .catch(err => {
-            res.status(400).send("unable to save to database");
-          });
+        .catch((err) => {
+            console.error(err);
+            next(err);
+        });
 });
 
-module.exports = AppointmentRouter
+router.delete('/appointment/:id', async (req, res) => { //Delete appointment by id
+    Appointment.remove({
+        id: req.params.id
+      }), function (err, appointment) {
+        if (err) {
+          return res.send(err);
+        }
+  
+        res.json({ message: 'Deleted' });
+      }; 
+});
+
+router.delete('/:identifier', (req, res) => {
+    
+});
+
+router.post('/appointment', async (req, res, next) => { //Add appointment
+    try {
+        const { description, doctor,patient,date } = req.body
+        const appointment = new Appointment({ description,doctor,patient,date })
+        const ret = await appointment.save()
+        res.json(ret)
+    } catch (error) {
+        return next(error)
+    }
+})
+
+module.exports = router
