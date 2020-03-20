b3fb2c41445cf6efeedf1e0ab0854eb8ce369f07
laszlobujak <laszlobujak@gmail.com>
2020-03-20 21:01:24 +0100

    Updated app to use the appointment route

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 require('./db/mongoose');
-// const userRouter = require("./routers/user");
+const userRouter = require('./routers/user');
+const appointmentRouter = require('./routers/appointment');
 
 const app = express();
 
 app.use(express.json());
-// app.use(userRouter);
+app.use(userRouter);
+app.use(appointmentRouter);
 
 module.exports = app;
