8504130adad47304cfef5894409b8f152a937d91
laszlobujak <laszlobujak@gmail.com>
2020-03-20 11:56:09 +0100

    Fixed React bug in statistics component

--- frontend/docvisitui/src/components/statistics/statistics.component.jsx
+++ frontend/docvisitui/src/components/statistics/statistics.component.jsx
@@ -1,48 +1,69 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
 //style
 import './statistic.style.scss';
 import CountUp from 'react-countup';
 import VisibilitySensor from 'react-visibility-sensor';
 
-const Statistics = () => {
-  const [visible, setVisible] = useState(false);
-  const [visible2, setVisible2] = useState(false);
+class Statistics extends Component {
+  constructor() {
+    super();
 
-  return (
-    <div className="statistic-container">
-      <div className="counter-box patient">
-        <i className="material-icons">accessibility</i>
-        <CountUp end={1200} redraw={true}>
-          {({ countUpRef, start }) => (
-            <VisibilitySensor onChange={start} delayedCall active={!visible}>
-              {({ isVisible }) => {
-                setVisible(isVisible);
-                return <span ref={countUpRef} />;
-              }}
-            </VisibilitySensor>
-          )}
-        </CountUp>
-        _____________________
-        <h5>Patients</h5>
+    this.state = {
+      active: true,
+      active2: true
+    };
+  }
+
+  render() {
+    return (
+      <div className="statistic-container">
+        <div className="counter-box patient">
+          <i className="material-icons">accessibility</i>
+          <CountUp end={1200} redraw={true}>
+            {({ countUpRef, start }) => (
+              <VisibilitySensor
+                onChange={isVisible => {
+                  start();
+                  this.setState({
+                    active: !isVisible
+                  });
+                }}
+                delayedCall
+                active={this.state.active}
+              >
+                {() => <span ref={countUpRef} />}
+              </VisibilitySensor>
+            )}
+          </CountUp>
+          _____________________
+          <h5>Patients</h5>
+        </div>
+
+        <div className="counter-box">
+          <i className="material-icons">people</i>
+          <CountUp end={29} redraw={true}>
+            {({ countUpRef, start }) => (
+              <VisibilitySensor
+                onChange={isVisible => {
+                  start();
+                  this.setState({
+                    active2: !isVisible
+                  });
+                }}
+                delayedCall
+                active={this.state.active2}
+              >
+                {() => <span ref={countUpRef} />}
+              </VisibilitySensor>
+            )}
+          </CountUp>
+          _____________________
+          <h5>Doctors</h5>
+        </div>
       </div>
-      <div className="counter-box">
-        <i className="material-icons">people</i>
-        <CountUp end={29} redraw={true}>
-          {({ countUpRef, start }) => (
-            <VisibilitySensor onChange={start} delayedCall active={!visible2}>
-              {({ isVisible }) => {
-                setVisible2(isVisible);
-                return <span ref={countUpRef} />;
-              }}
-            </VisibilitySensor>
-          )}
-        </CountUp>
-        _____________________
-        <h5>Doctors</h5>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Statistics;
