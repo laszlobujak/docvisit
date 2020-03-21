c7e8376361acb37a6f927f564797fe728c3734ff
laszlobujak <laszlobujak@gmail.com>
2020-03-21 10:43:53 +0100

    Removed useless coneole logs

--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -7,9 +7,7 @@ router.post('/users', async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    console.log('saved', user);
     const token = await user.generateToken();
-    console.log(token);
     res.status(201).send({ user, token });
   } catch (error) {
     res.status(400).send(error);
