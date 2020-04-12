040e4b3279df49e9ffc408e1f2f515c79e68c590
Buják László <laszlobujak@gmail.com>
2020-04-12 17:37:35 +0200

    Update mongoose.js

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/docvisit-api", {
+mongoose.connect(process.env.MONGO_URL, {
   useUnifiedTopology: true,
   useCreateIndex: true,
   useNewUrlParser: true
