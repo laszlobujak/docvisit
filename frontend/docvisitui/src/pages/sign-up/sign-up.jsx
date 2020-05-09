2b6d1fa4bd05f205cc049d515dd011fde64514ed
NovakDetti <54432325+NovakDetti@users.noreply.github.com>
2020-05-09 16:56:28 +0200

    Set up auth on sign up page

--- frontend/docvisitui/src/pages/sign-up/sign-up.jsx
+++ frontend/docvisitui/src/pages/sign-up/sign-up.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import axios from 'axios';
+import {Redirect} from 'react-router-dom';
 
 //styles
 import './sign-up.style.scss';
@@ -16,15 +17,21 @@ function SignUp(){
     event.preventDefault()
     axios.post('http://localhost:8000/users', { name, email, password })
       .then(result => {
-        if (result.status === 200) {
-          console.log(result)
-        }
+        if (result.status === 201) {
+              sessionStorage.setItem("token", result.data.token);
+              sessionStorage.setItem("user_id", result.data.user._id)
+          }
       })
       .catch((error) => console.log(error))
 
   }
 
-
+  if (sessionStorage.getItem("user_id")) {
+            return <Redirect to={{
+                pathname: '/account'
+            }}
+      />
+  }
     return (
       <div>
         <Nav></Nav>
@@ -32,60 +39,50 @@ function SignUp(){
           <h1 className="align-text">Sign up</h1>
           <div className="form-container">
             <form onSubmit={postSignup}>
-              <div class="form-group">
-                <label for="exampleInputEmail1">Full name</label>
+              <div className="form-group">
+                <label htmlFor="exampleInputEmail1">Full name</label>
                 <input
                   type="text"
                   value={name}
                   onChange={e => {
                     setName(e.target.value);
                   }}
-                  class="form-control"
+                  className="form-control"
                   id="fullName"
                   placeholder="Enter your name..."
                 ></input>
               </div>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Email address</label>
+              <div className="form-group">
+                <label htmlFor="exampleInputEmail1">Email address</label>
               <input
                 type="email"
                 value={email}
                 onChange={e => {
                   setEmail(e.target.value);
                 }}
-                class="form-control"
+                  className="form-control"
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
                 placeholder="Enter email"
               ></input>
-              <small id="emailHelp" class="form-text text-muted">
+              <small id="emailHelp" className="form-text text-muted">
                 We'll never share your email with anyone else.
               </small>
             </div>
-            <div class="form-group">
-              <label for="exampleInputPassword1">Password</label>
+              <div className="form-group">
+              <label htmlFor="exampleInputPassword1">Password</label>
               <input
                 type="password"
                 value={password}
                 onChange={e => {
                   setPassword(e.target.value);
                 }}
-                class="form-control"
+                  className="form-control"
                 id="exampleInputPassword1"
                 placeholder="Password"
               ></input>
             </div>
-            <div class="form-check">
-              <input
-                type="checkbox"
-                class="form-check-input"
-                id="exampleCheck1"
-              ></input>
-              <label class="form-check-label" for="exampleCheck1">
-                Privacy policy
-              </label>
-            </div>
-            <button type="submit" class="btn btn-outline-primary">
+              <button type="submit" className="btn btn-dark sign-up-button">
               Submit
             </button>
           </form>
