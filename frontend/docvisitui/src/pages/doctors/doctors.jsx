c7c2a611398bb332a0d751495fc36ddb0897b5c2
NovakDetti <54432325+NovakDetti@users.noreply.github.com>
2020-05-10 20:18:08 +0200

    Add nav bar on doctors page

--- frontend/docvisitui/src/pages/doctors/doctors.jsx
+++ frontend/docvisitui/src/pages/doctors/doctors.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import SearchBar from '../../components/searchbar/searchbar.component';
 import Card from '../../components/card/card.component';
+import {Link} from 'react-router-dom';
 
 //style
 import './doctors.style.scss';
@@ -24,6 +25,10 @@ const Doctors = () => {
     <div>
       <div className="top-container">
         <h2>Doctors</h2>
+        <div className="doctors-navigation-bar">
+          <div className="go-to-mainpage"><Link to='/'><span class="material-icons">home</span></Link></div>
+          <div className="go-to-mainpage"><Link to='/account'><span class="material-icons">account_circle</span></Link></div>
+        </div>
       </div>
       <SearchBar />
       <div className="doctor-cards">
